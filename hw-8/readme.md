0d12224cdf63c83d80e519bee8ff87118477fc76
Beau <toastman775@hotmail.com>
2018-03-15 20:18:47 -0600

    week 8

---
+++ hw-8/readme.md
@@ -0,0 +1,9 @@
+Beau Sutton, 50
+
+[Live Sketch](https://beausutton.github.io/120-work/hw-8/)
+
+# Week 8 Readme
+
+This week was moderately easy, as in, no real problems like the ones i've been having the past weeks. However, these types of loops seem much more skill based instead of just rewriting every line of code to accomplish the same goal. I have nothing wrong with it, it's just that I need to still improve my skills with understanding variables and certain actions. Luckily, I had no substantial problems with this weeks homework, unlike last week, and it seems as if everyone has the same opinion because I don't think I received any emails about students needing help.
+
+Furthermore, I'm going to explore more about these loops over the weekend. I've been neglecting my reading from the textbook so this weekend will be a good time to get caught up and hone my skills. 
---
+++ hw-8/sketch.js
@@ -0,0 +1,42 @@
+//preping variables
+let rows;
+let colums;
+let x_space;
+let y_space;
+//setting up the canvas as well as assigning values to the variables
+function setup() {
+	createCanvas( windowWidth, 800 );
+    frameRate(6);
+
+	  x_space = 29;
+	  rows = floor(width/x_space);
+	  y_space = 30;
+    colums = floor(height/y_space);
+}
+
+function draw() {
+//creating a background that will randomly change colors
+  background(random(5,245),random(50,150), 200);
+    noStroke();
+    
+	  let x = 2.5;
+	  let y = 5;
+//creating the conditions for random ellipses to appear on screen
+while (y!= 0){
+    x = floor(random() * rows);
+		y = floor(random() * colums);
+//ellipses will randomly switch between colors
+		fill(200,random(5,205),random(50,250));
+//multiple different ellipses that all have differnt sizes
+    ellipse(x*x_space,y*y_space,random(15, 40));
+
+    ellipse(x*x_space,y*y_space,random(20, 90));
+
+    ellipse(x*x_space,y*y_space,random(5, 20));
+
+
+  }
+
+
+
+}
